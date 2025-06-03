7d24c6cfa6e0380eba12f104382c1f47f35dddc7
Dominik Kundel <dkundel@openai.com>
2025-06-03 16:01:52 -0700

    following along

--- 01-basic/index.ts
+++ 01-basic/index.ts
@@ -1,1 +1,24 @@
+import { Agent, tool, run } from "@openai/agents";
+import z from "zod";
 
+const getWeather = tool({
+  name: "getWeather",
+  description: "Get the weather in a given location",
+  parameters: z.object({
+    location: z.string(),
+  }),
+  execute: async ({ location }) => {
+    return `The weather in ${location} is sunny`;
+  },
+});
+
+const agent = new Agent({
+  name: "My Agent",
+  instructions: "You are a helpful assistant.",
+  model: "o4-mini",
+  tools: [getWeather],
+});
+
+const result = await run(agent, "What is the weather in Tokyo?");
+
+console.log(result.finalOutput);
--- 02-voice/src/app/page.tsx
+++ 02-voice/src/app/page.tsx
@@ -1,14 +1,31 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { RealtimeAgent, RealtimeSession } from "@openai/agents/realtime";
+import { getSessionToken } from "./server/token";
+
+const agent = new RealtimeAgent({
+  name: "Voice Agent",
+  instructions:
+    "You are a voice agent that can answer questions and help with tasks.",
+});
 
 export default function Home() {
+  const session = useRef<RealtimeSession | null>(null);
   const [connected, setConnected] = useState(false);
 
   async function onConnect() {
     if (connected) {
       setConnected(false);
+      await session.current?.close();
     } else {
+      const token = await getSessionToken();
+      session.current = new RealtimeSession(agent, {
+        model: "gpt-4o-realtime-preview-2025-06-03",
+      });
+      await session.current.connect({
+        apiKey: token,
+      });
       setConnected(true);
     }
   }
